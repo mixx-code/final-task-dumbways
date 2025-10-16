0585fe494901bf5d1612c3d2ed27d20e4f47f6f9
mixx-code <rfebriansyah385@gmail.com>
2025-10-16 21:52:02 +0700

    db deploy

--- db.js
+++ db.js
@@ -3,16 +3,14 @@ import { Pool } from "pg";
 let pool;
 
 if (process.env.DATABASE_URL) {
-  // 🌐 Koneksi Neon / Vercel Postgres (production)
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false, // Neon butuh SSL
+      rejectUnauthorized: false,
     },
   });
   console.log("✅ Connected to Neon/PostgreSQL (via DATABASE_URL)");
 } else {
-  // 💻 Koneksi lokal (development)
   pool = new Pool({
     user: "postgres",
     host: "localhost",
